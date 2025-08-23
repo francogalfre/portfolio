a2c24ee4c452bc6a4fb7ed8c0ecfe2cb7b285974
francogalfre <francogalfre.code@gmail.com>
2025-08-23 01:20:29 -0300

    feat(i18n): :sparkles: add button to change the languague

--- src/i18n/translations/es/hero.ts
+++ src/i18n/translations/es/hero.ts
@@ -1,5 +1,5 @@
 export const hero = {
-  pill: "Disponible",
+  pill: "Buscando nuevas oportunidades",
   description:
     "Estudiante de Ingeniería de Software creando experiencias web modernas con Next.js, React y Typescript. He ayudado a 25+ clientes alrededor del mundo a hacer realidad sus ideas digitales mediante código limpio y diseño cuidadoso.",
   CVbutton: "Descargar CV",
