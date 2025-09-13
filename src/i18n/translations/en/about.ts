c64335d5299b9dbb26a79a9bba11b50bb7f92df6
francogalfre <francogalfre.code@gmail.com>
2025-09-13 19:53:18 -0300

    feat(about): :speech_balloon: add more information about me

--- src/i18n/translations/en/about.ts
+++ src/i18n/translations/en/about.ts
@@ -1,7 +1,13 @@
+const today = new Date();
+let age = today.getFullYear() - 2007;
+
+if (today.getMonth() < 1 || (today.getMonth() === 1 && today.getDate() < 22)) {
+  age--;
+}
+
 export const about = {
   title: "About me",
-  paragraph1:
-    "I'm Franco! 🍷 A passionate developer who loves computing, coding, and coffee. I approach every challenge with enthusiasm and curiosity, always excited to learn new technologies.",
+  paragraph1: `I'm Franco! 🍷 I'm ${age} years old and a passionate developer who loves computing, coding, and coffee. I approach every challenge with enthusiasm and curiosity, always excited to learn new technologies.`,
   paragraph2:
     "I thrive in collaborative environments and enjoy tackling complex problems. Je parle trois langues and play chess in my free time, which helps with strategic thinking and problem-solving.",
 };
--- src/i18n/translations/es/about.ts
+++ src/i18n/translations/es/about.ts
@@ -1,7 +1,13 @@
+const today = new Date();
+let age = today.getFullYear() - 2007;
+
+if (today.getMonth() < 1 || (today.getMonth() === 1 && today.getDate() < 22)) {
+  age--;
+}
+
 export const about = {
   title: "Sobre mi",
-  paragraph1:
-    "Soy Franco! 🍷 Un desarrollador apasionado al que le encantan la informática, la programación y el café. Enfrento cada desafío con entusiasmo y curiosidad, siempre con ganas de aprender nuevas tecnologías.",
+  paragraph1: `Soy Franco! 🍷 Tengo ${age} años y soy un desarrollador apasionado al que le encantan la informática, la programación y el café. Enfrento cada desafío con entusiasmo y curiosidad, siempre con ganas de aprender nuevas tecnologías.`,
   paragraph2:
     "Me desenvuelvo muy bien en entornos colaborativos y disfruto resolver problemas complejos. Hablo tres idiomas y juego al ajedrez en mi tiempo libre, lo que me ayuda con el pensamiento estratégico y la resolución de problemas.",
 };
