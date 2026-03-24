953215bc56b1ab58676624e3cd1d3648b71d14d9
francogalfre <francogalfre.code@gmail.com>
2026-03-24 12:20:55 -0300

    chore(SEO): :mag: implement keywords meta tag

--- src/data/seo-content.ts
+++ src/data/seo-content.ts
@@ -1,6 +1,7 @@
 export interface PageSEO {
   title: string;
   description: string;
+  keywords: string;
 }
 
 export const SITE_URL = "https://francogalfre.vercel.app";
@@ -10,4 +11,6 @@ export const seoData: PageSEO = {
   title: "Franco Galfré",
   description:
     "Full-stack developer building digital products and learning everyday",
+  keywords:
+    "fullstack, frontend, backend, dev, developer, coding, designer, typescript, nextjs, francogalfre",
 };
