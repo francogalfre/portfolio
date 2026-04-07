014970d3fce8934fc5dadbc2ce3f2a63a98a9e92
francogalfre <francogalfre.code@gmail.com>
2026-04-07 12:09:27 -0300

    feat(games): :sparkles: add Doom

--- src/data/games-content.ts
+++ src/data/games-content.ts
@@ -2,13 +2,16 @@ export const games = [
   {
     name: "Sonic the Hedgehod",
     href: "sonic",
-    image:
-      "https://i.guim.co.uk/img/media/0c37b0e2fa73d085bac569d87e8163c25b6a52b8/0_30_640_384/master/640.jpg?width=1200&quality=85&auto=format&fit=max&s=8b2d28411cce7e11291b750dc821803a",
+    image: "/games/sonic.webp",
   },
   {
     name: "Mortal Kombat",
     href: "mortal-kombat",
-    image:
-      "https://blogscdn.thehut.net/app/uploads/sites/39/2022/10/Untitled-design-20_1664807258.jpg",
+    image: "/games/mk.webp",
+  },
+  {
+    name: "Doom",
+    href: "doom",
+    image: "/games/doom.webp",
   },
 ];
