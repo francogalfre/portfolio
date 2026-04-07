4319f2c54c0a09d6cf855c0d0f8a3e7483d44ce3
francogalfre <francogalfre.code@gmail.com>
2026-04-07 11:36:56 -0300

    feat(games): :sparkles: implement game page with konami code

---
+++ src/data/games-content.ts
@@ -0,0 +1,14 @@
+export const games = [
+  {
+    name: "Sonic the Hedgehod",
+    href: "sonic",
+    image:
+      "https://i.guim.co.uk/img/media/0c37b0e2fa73d085bac569d87e8163c25b6a52b8/0_30_640_384/master/640.jpg?width=1200&quality=85&auto=format&fit=max&s=8b2d28411cce7e11291b750dc821803a",
+  },
+  {
+    name: "Mortal Kombat",
+    href: "mortal-kombat",
+    image:
+      "https://blogscdn.thehut.net/app/uploads/sites/39/2022/10/Untitled-design-20_1664807258.jpg",
+  },
+];
